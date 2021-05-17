6bff741c9b88a7daa9b87d0104ac8da026730e21
Vinh Phuc Le <lvphuc@pop-os.localdomain>
2021-05-17 16:40:25 +0700

    Add render Cinema & FindMovieBox

--- src/Pages/Carousel/FindMovieBox.js
+++ src/Pages/Carousel/FindMovieBox.js
@@ -1,47 +1,163 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getDetailMovieAction } from "../../redux/Action/MovieAction";
 
 export default function FindMovieBox() {
-    const { allMovie } = useSelector(state => state.MovieReducer)
+  const [chosenCinemaBrand, setchosenCinemaBrand] = useState("");
+  const [chosenCinema, setchosenCinema] = useState("");
+  const [chosenDate, setchosenDate] = useState("");
+  const { allMovie, detailMovie } = useSelector((state) => state.MovieReducer);
 
-   
-    return (
-        <div className="container">
-            <div style={{ width: '100%' }} class="form-group d-flex " id="findMovieTools">
-                <div className="findMovieBox">
-                    <select class="form-control form-control-lg " id="exampleFormControlSelect1"  >
-                        <option value="" hidden >Choose Movie</option>
-                     
-                    </select>
-                    <select class="form-control form-control-lg" id="exampleFormControlSelect1">
-                        <option value="" hidden >Cinema</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                    </select>
-                    <select class="form-control form-control-lg" id="exampleFormControlSelect1">
-                        <option value="" hidden >Date</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                    </select>
-                    <select class="form-control form-control-lg" id="exampleFormControlSelect1">
-                        <option value="" hidden >When</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                    </select>
-                </div>
+  const dispatch = useDispatch();
 
-                <button className="btn btn-secondary">Buy Ticket</button>
-            </div>
+  function removeDuplicates(array) {
+    let a = [];
+    array?.map((x, index) => {
+      if (!a.includes(x)) {
+        a.push(x);
+      }
+    });
+    return a;
+  }
+
+  const handleChange = (e) => {
+    let {
+      findBoxChooseCinema,
+      findBoxChooseMovie,
+      findBoxChooseDate,
+      findBoxChooseWhen,
+    } = e.target;
+  };
+
+  const renderChooseMovie = () => {
+    return allMovie.map((movie, index) => {
+      return (
+        <option value={movie.maPhim} key={index}>
+          {movie.tenPhim}
+        </option>
+      );
+    });
+  };
+  const renderChooseCinemaBrand = () => {
+    return detailMovie.heThongRapChieu?.map((cinemaBrand, index) => {
+      return (
+        <option value={cinemaBrand.maHeThongRap} key={index}>
+          {cinemaBrand.tenHeThongRap}
+        </option>
+      );
+    });
+  };
+  const renderChooseCinema = () => {
+    return chosenCinemaBrand.cumRapChieu?.map((cinema, index) => {
+      return (
+        <option value={cinema.maCumRap} key={index}>
+          {cinema.tenCumRap}
+        </option>
+      );
+    });
+  };
+  const renderChooseDate = () => {
+    let DateTimeArr = chosenCinema.lichChieuPhim?.map((item, index) => {
+      return item.ngayChieuGioChieu.split("T")[0];
+    });
+    let DateArr = removeDuplicates(DateTimeArr);
+    return DateArr.map((item, index) => {
+      return (
+        <option value={item} key={index}>
+          {item}
+        </option>
+      );
+    });
+  };
+  const renderChooseTime = () => {
+    let DateTimeArr = chosenCinema.lichChieuPhim?.map((item, index) => {
+      return item.ngayChieuGioChieu.split("T");
+    });
+    return DateTimeArr?.filter((item) => item[0] === chosenDate).map(
+      (item, index) => {
+        return (
+          <option value={item[1]} key={index}>
+            {item[1]}
+          </option>
+        );
+      }
+    );
+  };
+  return (
+    <div className="container">
+      <div
+        style={{ width: "100%" }}
+        class="form-group d-flex "
+        id="findMovieTools"
+      >
+        <div className="findMovieBox">
+          <select
+            class="form-control form-control-lg "
+            name="findBoxChooseMovie"
+            onChange={(e) => {
+              dispatch(getDetailMovieAction(e.target.value));
+            }}
+          >
+            <option value="" hidden>
+              Choose Movie
+            </option>
+            {renderChooseMovie()}
+          </select>
+          <select
+            class="form-control form-control-lg "
+            name="findBoxChooseMovie"
+            onChange={(e) => {
+              setchosenCinemaBrand(
+                detailMovie.heThongRapChieu.find(
+                  (item) => item.maHeThongRap === e.target.value
+                )
+              );
+              //   dispatch(getDetailMovieAction(chosenMovie));
+            }}
+          >
+            <option value="" hidden>
+              Choose Cinema Brand
+            </option>
+            {renderChooseCinemaBrand()}
+          </select>
+          <select
+            class="form-control form-control-lg"
+            name="findBoxChooseCinema"
+            onChange={(e) => {
+              setchosenCinema(
+                chosenCinemaBrand.cumRapChieu.find(
+                  (item) => item.maCumRap === e.target.value
+                )
+              );
+            }}
+          >
+            <option value="" hidden>
+              Cinema
+            </option>
+            {renderChooseCinema()}
+          </select>
+          <select
+            class="form-control form-control-lg"
+            name="findBoxChooseDate"
+            onChange={(e) => {
+              setchosenDate(e.target.value);
+            }}
+          >
+            <option value="" hidden>
+              Date
+            </option>
+            {renderChooseDate()}
+          </select>
+          <select class="form-control form-control-lg" name="findBoxChooseWhen">
+            <option value="" hidden>
+              When
+            </option>
+            {renderChooseTime()}
+          </select>
         </div>
 
-
-
-
-    )
+        <button className="btn btn-secondary">Buy Ticket</button>
+      </div>
+    </div>
+  );
 }
--- src/redux/Action/MovieAction.js
+++ src/redux/Action/MovieAction.js
@@ -1,57 +1,55 @@
-import axios from 'axios'
-import { method } from 'lodash'
-import { GET_ALL_MOVIE } from '../../util/Setting'
+import axios from "axios";
+import { method } from "lodash";
+import { GET_ALL_MOVIE } from "../../util/Setting";
 
-
-export const getAllMovieAction = () => { // Tai khoan va mat khau
-    return async (dispatch) => {
-        try {
-            const result = await axios({
-                url: 'https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP02',
-                method: 'GET',
-            })
-            dispatch({
-                type: GET_ALL_MOVIE,
-                allMovie: result.data
-            })
-        } catch (errors) {
-            console.log('errors', errors.response?.data)
-        }
+export const getAllMovieAction = () => {
+  // Tai khoan va mat khau
+  return async (dispatch) => {
+    try {
+      const result = await axios({
+        url: "https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP02",
+        method: "GET",
+      });
+      dispatch({
+        type: GET_ALL_MOVIE,
+        allMovie: result.data,
+      });
+    } catch (errors) {
+      console.log("errors", errors.response?.data);
     }
-}
+  };
+};
 
 export const getDetailMovieAction = (maPhim) => {
-    return async (dispacth) => {
-        try {
-            const result = await axios({
-                url: `https://movie0706.cybersoft.edu.vn/api/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${maPhim}`,
-                method: 'GET'
-            })
-            dispacth({
-                type: 'GET_DETAIL_MOVIE',
-                detailMovie: result.data
-            })
-
-        } catch (errors) {
-            console.log('errors', errors.response?.data)
-        }
+  return async (dispacth) => {
+    try {
+      const result = await axios({
+        url: `https://movie0706.cybersoft.edu.vn/api/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${maPhim}`,
+        method: "GET",
+      });
+      dispacth({
+        type: "GET_DETAIL_MOVIE",
+        detailMovie: result.data,
+      });
+    } catch (errors) {
+      console.log("errors", errors.response?.data);
     }
-}
+  };
+};
 
 export const getAllMoviePage = (page) => {
-    return async (dispacth) => {
-        try {
-            const result = await axios({
-                url: `https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhimPhanTrang?maNhom=GP02&soTrang=${page}&soPhanTuTrenTrang=10`,
-                method: 'GET'
-            })
-            dispacth({
-                type: "GET-ALL-MOVIE-PAGE",
-                allMoviePage: result.data
-            })
-
-        } catch (errors) {
-            console.log('errors', errors.response?.data)
-        }
+  return async (dispacth) => {
+    try {
+      const result = await axios({
+        url: `https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhimPhanTrang?maNhom=GP02&soTrang=${page}&soPhanTuTrenTrang=10`,
+        method: "GET",
+      });
+      dispacth({
+        type: "GET-ALL-MOVIE-PAGE",
+        allMoviePage: result.data,
+      });
+    } catch (errors) {
+      console.log("errors", errors.response?.data);
     }
-}
+  };
+};
